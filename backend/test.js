cdf34d44a0b6f715cad8341d976717970f732bf9
Amna Ahmed <ahmadalisukhera7849@gmail.com>
2026-04-24 22:13:11 +0500

    Setup a skeleton server.js, ran a mock sanity check

---
+++ backend/test.js
@@ -0,0 +1,20 @@
+const payload = {
+    patientID: "PT_123",
+    sequences: [
+        { gene: "CFTR", sequenceData: "ATTGTC" },//correct
+        { gene: "HBB", sequenceData: "hasghgasyu" },//wrong sequence
+        { gene: "Fake", sequenceData: "csds" }//fake gene
+    ]
+};
+
+console.log("Sending off data")
+fetch('http://localhost:3000/api/analyze_patients', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log("Server sent this message");
+        console.log(data)
+    }).catch(error => console.error("Not delivered", error.message))
